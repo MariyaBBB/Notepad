85760aefc28cea31180e2abfaef3eb548370aa65
Мария Моисеева <mash.moi@icloud.com>
2022-09-21 10:50:02 +0300

    Создание страницы со списком постов

---
+++ src/pages/Posts.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ArticleList from "../components/Articles/ArticleList";
+
+const Posts = () => {
+    return (
+        <ArticleList />
+    )
+}
+
+export default Posts;
