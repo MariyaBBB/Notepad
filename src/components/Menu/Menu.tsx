47372788388f97a9db5397ee1bc56d2e1f417305
Мария Моисеева <mash.moi@icloud.com>
2022-10-11 14:29:26 +0300

    Создание компонента Menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MenuElement from "./MenuElement";
 import { menuElements, IMenuItem } from "../../data/menuElements";
 
-const Menu = () => {
+interface IMenuProps {
+  menuOpen: boolean
+}
+
+const Menu = ({menuOpen}: IMenuProps ) => { 
+  const lastElement = menuElements.length - 1
   return (
+    <>    
     <menu
-      className="sm:max-w-fit sm:grid sm:grid-flow-col hidden sm:content-center sm:justify-self-end sm:gap-5 sm:mt-0 mt-7"
+      className={menuOpen ? 'show mt-7 sm:max-w-fit sm:grid sm:grid-flow-col sm:content-center sm:justify-self-end sm:gap-5 sm:mt-0' : "hidden sm:max-w-fit sm:grid sm:grid-flow-col sm:content-center sm:justify-self-end sm:gap-5 sm:mt-0 mt-7" }
     >
       {menuElements.map((item: IMenuItem) => (
         <MenuElement
+          isLast={menuElements.indexOf(item) === lastElement}
           key={item.link}
           icon={item.icon}
           name={item.name}
@@ -16,6 +23,8 @@ const Menu = () => {
         />
       ))}
     </menu>
+
+    </>
   );
 };
 
