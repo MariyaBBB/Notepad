7d9b77354ae37a78872d91000d85c686b6ccc4af
Мария Моисеева <mash.moi@icloud.com>
2022-10-11 14:28:58 +0300

    Cоздание компонента Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Menu from './Menu/Menu'
 
@@ -7,6 +8,17 @@ interface HeaderProps {
 }
  
 const Header: React.FunctionComponent<HeaderProps> = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    function openOrClose(e: any) {
+        if(menuOpen === false) {
+            e.target.src = 'images/icons/menu-close.svg'
+            setMenuOpen(true)
+        } else {
+            setMenuOpen(false)
+            e.target.src = 'images/icons/menu.svg'
+        }
+
+    }
     return (
         <div className="sm:grid sm:grid-cols-2 sm:m-auto sm:mt-10 mt-6 ml-5 mr-5">
             <div className="grid grid-cols-2 ">
@@ -15,9 +27,11 @@ const Header: React.FunctionComponent<HeaderProps> = () => {
                   className="sm:max-w-185 sm:text-4xl text-3xl text-green-bright font-medium  sm:inline"
                   >Блокнотик
                   </Link>
-                <img id="menu_button" alt="" className="sm:hidden justify-self-end cursor-pointer" src="images/icons/menu.svg" />
+                <img id="menu_button" 
+                onClick={openOrClose}
+                alt="" className="sm:hidden justify-self-end cursor-pointer" src="images/icons/menu.svg" />
             </div>
-            <Menu />
+            <Menu menuOpen={menuOpen}/>
             
         </div>
     )
